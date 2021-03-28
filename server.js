70541afc69c3b999c9501f89c97fe8b8e2f4334b
sonikvvv <51079681+sonikvvv@users.noreply.github.com>
2021-03-28 18:26:18 +0300

    New GET requests.

--- server.js
+++ server.js
@@ -1,10 +1,28 @@
 const express = require("express");
 const app = express();
+const ejsMate = require("ejs-mate");
 
+app.engine('ejs', ejsMate);
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-    res.render("index")
+    res.render("home");
 });
 
-app.listen(3000)
+app.get("/badges", (req, res) => {
+    res.render("badges/badges");
+});
+
+app.get("/rules", (req, res) => {
+    res.render("rules/rules");
+});
+
+app.get("/login", (req, res) => {
+    res.render("log-reg/login");
+});
+
+app.get("/register", (req, res) => {
+    res.render("log-reg/register");
+});
+
+app.listen(3000);
