86323426db497adab63578800a4233f0aab63d28
sonikvvv <51079681+sonikvvv@users.noreply.github.com>
2021-04-07 17:57:09 +0300

    Updated the routs to catch async errors.

--- routes/badges.js
+++ routes/badges.js
@@ -1,42 +1,43 @@
 const express = require("express");
 const Badge = require("../models/badgeModel");
+const catchAsync = require('../utils/catchAsync');
 const router = express.Router();
 
-router.get("/", async (req, res) => {
+router.get("/", catchAsync( async (req, res) => {
     const badges = await Badge.find({});
     res.render("badges/badges", { badges });
-});
+}));
 
 router.get("/new", (req, res) => {
     res.render("badges/new");
 });
 
-router.get("/:id", async (req, res) => {
+router.get("/:id", catchAsync( async (req, res) => {
     const badge = await Badge.findById(req.params.id);
     res.render("badges/singleBadge", { badge });
-});
+}));
 
-router.get("/:id/edit", async (req, res) => {
+router.get("/:id/edit", catchAsync( async (req, res) => {
     const badge = await Badge.findById(req.params.id);
     res.render("badges/edit", { badge });
-});
+}));
 
-router.post("/", async (req, res) => {
+router.post("/", catchAsync( async (req, res) => {
     const badge = new Badge(req.body.badge);
     await badge.save();
     res.redirect(`/badges/${badge._id}`);
-});
+}));
 
-router.put("/:id", async (req, res) => {
+router.put("/:id", catchAsync( async (req, res) => {
     const { id } = req.params;
     const badge = await Badge.findByIdAndUpdate(id, { ...req.body.badge });
     res.redirect(`/badges/${badge._id}`);
-});
+}));
 
-router.delete("/:id", async (req, res) => {
+router.delete("/:id", catchAsync( async (req, res) => {
     const { id } = req.params;
     const badge = await Badge.findByIdAndDelete(id);
     res.redirect("/badges");
-});
+}));
 
 module.exports = router;
--- routes/rules.js
+++ routes/rules.js
@@ -1,42 +1,43 @@
 const express = require("express");
-const router = express.Router();
 const Rule = require("../models/ruleModel");
+const catchAsync = require('../utils/catchAsync');
+const router = express.Router();
 
-router.get("/", async (req, res) => {
+router.get("/", catchAsync( async (req, res) => {
     const rules = await Rule.find({});
     res.render("rules/rules", { rules });
-});
+}));
 
 router.get("/new", (req, res) => {
     res.render("rules/newRules");
 });
 
-router.post("/", async (req, res) => {
+router.post("/", catchAsync( async (req, res) => {
     const rule = new Rule({ ...req.body.rule });
     await rule.save();
     res.redirect(`/rules/${rule._id}`);
-});
+}));
 
-router.get("/:id", async (req, res) => {
+router.get("/:id", catchAsync( async (req, res) => {
     const rule = await Rule.findById(req.params.id);
     res.render("rules/rule", { rule });
-});
+}));
 
-router.get("/:id/edit", async (req, res) => {
+router.get("/:id/edit", catchAsync( async (req, res) => {
     const rule = await Rule.findById(req.params.id);
     res.render("rules/edit", { rule });
-});
+}));
 
-router.put("/:id", async (req, res) => {
+router.put("/:id", catchAsync( async (req, res) => {
     const { id } = req.params;
     const rule = await Rule.findByIdAndUpdate(id, { ...req.body.rule });
     res.redirect(`/rules/${rule._id}`);
-});
+}));
 
-router.delete("/:id", async (req, res) => {
+router.delete("/:id", catchAsync( async (req, res) => {
     const { id } = req.params;
     const rule = await Rule.findByIdAndDelete(id);
     res.redirect("/rules");
-});
+}));
 
 module.exports = router;
