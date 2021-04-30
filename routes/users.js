73770f72040835aa7b8b9f901f8101edec05915d
Fresh71 <78268552+Fresh71@users.noreply.github.com>
2021-04-30 13:09:46 +0300

    Added discover page filter for users.

--- routes/users.js
+++ routes/users.js
@@ -1,31 +1,45 @@
 const express = require("express");
 const { isError } = require("joi"); //? do we use it
-const User = require('../utils/models/userModel');
-const catchAsync = require('../utils/catchAsync');
+const User = require("../utils/models/userModel");
+const catchAsync = require("../utils/catchAsync");
 const router = express.Router();
-const passport = require('passport');
-const { validateUser, validateRegister, isLoggedIn } = require("../utils/middleware");
+const passport = require("passport");
+const {
+    validateUser,
+    validateRegister,
+    isLoggedIn,
+} = require("../utils/middleware");
 
-router.get("/", catchAsync( async (req, res) => {
-    const users = await User.find({});
-    res.render("users/users", { users });
-}));
+router.get(
+    "/",
+    catchAsync(async (req, res) => {
+        const users = await User.find({});
+        res.render("users/users", { users });
+    })
+);
 
 router.get("/login", (req, res) => {
     res.render("users/log-reg/login");
 });
 
-router.post("/login", passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
-    const user = req.user;
-    req.flash("success", `Welcome back ${user.username}!`);
-    const redirect = req.session.returnTo || '/users/discover';
-    res.redirect(redirect);
-});
+router.post(
+    "/login",
+    passport.authenticate("local", {
+        failureFlash: true,
+        failureRedirect: "/login",
+    }),
+    (req, res) => {
+        const user = req.user;
+        req.flash("success", `Welcome back ${user.username}!`);
+        const redirect = req.session.returnTo || "/users/discover";
+        res.redirect(redirect);
+    }
+);
 
 router.get("/logout", (req, res) => {
     req.logout();
-    req.flash('success', 'Goodbye!');
-    res.redirect('/');
+    req.flash("success", "Goodbye!");
+    res.redirect("/");
 });
 
 router.get("/register", (req, res) => {
@@ -40,55 +54,98 @@ router.post(
             const { email, username, password, years } = req.body.user;
             const user = new User({ email, username, years });
             const registeredUser = await User.register(user, password);
-            req.login(registeredUser, err => {
+            req.login(registeredUser, (err) => {
                 if (err) return next(err);
-                req.flash('success', 'Welcome to BlindDatee!');
+                req.flash("success", "Welcome to BlindDatee!");
                 res.redirect(`/users/${user._id}`);
             });
         } catch (e) {
-            req.flash('error', e.message);
-            res.redirect('/users/register');
+            req.flash("error", e.message);
+            res.redirect("/users/register");
         }
     })
 );
 
 router.get(
-    '/chats', isLoggedIn,
+    "/chats",
+    isLoggedIn,
     catchAsync(async (req, res) => {
         const users = await User.find({});
-        res.render('users/chats/chatsPage', { users });
+        res.render("users/chats/chatsPage", { users });
     })
 );
 
-router.get('/discover', isLoggedIn, (req, res) => {
-    res.render('users/discover/discoverPage');
+router.get("/discover", isLoggedIn, (req, res) => { // TODO: visualize the users
+    if (req.session.filterResult){
+        const users = req.session.filterResult;
+        return res.render("users/discover/discoverPage");
+    }
+    res.render("users/discover/discoverPage");
 });
 
-router.post('/discover', isLoggedIn, (req, res) => {
-    res.send(req.body.user);
-});
+router.post(
+    "/discover",
+    isLoggedIn,
+    catchAsync(async (req, res) => {
+        const {
+            sex,
+            orientation,
+            years_from,
+            years_to,
+            relationshipStatus,
+            bodyType,
+        } = req.body.user;
 
-router.get("/:id", catchAsync( async (req, res) => {
-    const { id } = req.params;
-    const user = await User.findById(id);
-    res.render("users/userPage", { user });
-}));
+        // checking if the're is data & making the filter
+        let filter = {};
+        if (sex) filter.sex = sex;
+        if (orientation) filter.orientation = orientation;
+        if (years_from && years_to) {
+            filter.years = { $gte: years_from, $lte: years_to };
+        } 
+        if (relationshipStatus) filter.relationshipStatus = relationshipStatus;
+        if (bodyType) filter.bodyType = bodyType;
 
-router.get("/:id/edit", catchAsync( async (req, res) => {
-    const user = await User.findById(req.params.id);
-    res.render("users/edit", { user });
-}));
+        const users = await User.find({ ...filter });
+        req.session.filterResult = users;
+        res.redirect('/users/discover');
+    })
+);
 
-router.put("/:id", validateUser, catchAsync( async (req, res) => {
-    const { id } = req.params;
-    const user = await User.findByIdAndUpdate(id, { ...req.body.user });
-    res.render("users/userPage", { user });
-}));
+router.get(
+    "/:id",
+    catchAsync(async (req, res) => {
+        const { id } = req.params;
+        const user = await User.findById(id);
+        res.render("users/userPage", { user });
+    })
+);
 
-router.delete("/:id", catchAsync( async (req, res) => {
-    const { id } = req.params;
-    const user = await User.findByIdAndDelete(id);
-    res.redirect("/users");
-}));
+router.get(
+    "/:id/edit",
+    catchAsync(async (req, res) => {
+        const user = await User.findById(req.params.id);
+        res.render("users/edit", { user });
+    })
+);
+
+router.put(
+    "/:id",
+    validateUser,
+    catchAsync(async (req, res) => {
+        const { id } = req.params;
+        const user = await User.findByIdAndUpdate(id, { ...req.body.user });
+        res.render("users/userPage", { user });
+    })
+);
+
+router.delete(
+    "/:id",
+    catchAsync(async (req, res) => {
+        const { id } = req.params;
+        const user = await User.findByIdAndDelete(id);
+        res.redirect("/users");
+    })
+);
 
 module.exports = router;
