b1c6979efa8f4ecdd73ce51f1f61adca755d844f
Fresh71 <78268552+Fresh71@users.noreply.github.com>
2021-04-30 11:35:08 +0300

    Populating database with test data.

--- utils/seeds/index.js
+++ utils/seeds/index.js
@@ -1,6 +1,10 @@
 const mongoose = require("mongoose");
-const Tag = require("../../models/tagModel");
-const tagSeeds = require('./tagSeeds');
+const Tag = require("../models/tagModel");
+const Rule = require("../models/ruleModel");
+const Badge = require("../models/badgeModel");
+const badgeSeeds = require("./badgeSeeds");
+const ruleSeeds = require("./ruleSeeds");
+const tagSeeds = require("./tagSeeds");
 
 mongoose.connect("mongodb://localhost:27017/dating-app", {
     useNewUrlParser: true,
@@ -18,10 +22,30 @@ const seedDB = async () => {
     await Tag.deleteMany({});
     for (let i = 0; i < tagSeeds.length; i++) {
         const tag = new Tag({
-            title: tagSeeds[i].title
+            title: tagSeeds[i].title,
         });
         await tag.save();
     }
-}
+
+    await Rule.deleteMany({});
+    for (let i = 0; i < ruleSeeds.length; i++) {
+        const rule = new Rule({
+            title: ruleSeeds[i].title,
+            description: ruleSeeds[i].description,
+        });
+        await rule.save();
+    }
+
+    await Badge.deleteMany({});
+    for (let i = 0; i < badgeSeeds.length; i++) {
+        const badge = new Badge({
+            title: badgeSeeds[i].title,
+            description: badgeSeeds[i].description,
+            image: badgeSeeds[i].image,
+        });
+        await badge.save();
+    }
+    console.log("FINITO");
+};
 
 seedDB();
