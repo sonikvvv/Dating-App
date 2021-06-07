cd3b8f3e650da02998e92944e2bf568b37b76a75
sonikvvv <51079681+sonikvvv@users.noreply.github.com>
2021-06-07 16:02:46 +0300

    User images add and delete.

--- routes/settings.js
+++ routes/settings.js
@@ -4,16 +4,20 @@ const User = require("../utils/models/userModel");
 const catchAsync = require("../utils/catchAsync");
 const passport = require("passport");
 const multer = require("multer");
-const { storage } = require("../cloudinary");
+const { storage, cloudinary } = require("../cloudinary");
 const upload = multer({ storage });
 const { isLoggedIn } = require("../utils/middleware");
 
-router.get("/", isLoggedIn, catchAsync(async (req, res) => {
-    const user = await User.findById(req.user._id).populate('images');
-    res.render("users/settings/settings", { user });
-}));
+router.get(
+    "/",
+    isLoggedIn,
+    catchAsync(async (req, res) => {
+        const user = await User.findById(req.user._id).populate("images");
+        res.render("users/settings/settings", { user });
+    })
+);
 
-router.get("/filter/edit", isLoggedIn, (req, res) => {
+router.get("/filter", isLoggedIn, (req, res) => {
     res.render("users/settings/filter");
 });
 
@@ -26,6 +30,51 @@ router.post(
 
         await User.findByIdAndUpdate(user._id, { ...user });
         res.redirect("/users/discover");
+    })
+);
+
+router.get("/images", isLoggedIn, (req, res) => {
+    const user = req.user;
+    res.render("users/settings/images", { user });
+});
+
+router.post(
+    "/images",
+    upload.array("images"),
+    isLoggedIn,
+    catchAsync(async (req, res) => {
+        const images = req.files.map((f) => ({
+            url: f.path,
+            filename: f.filename,
+        }));
+        const deleteImages = req.body.deleteImages
+            ? [...req.body.deleteImages]
+            : [];
+
+        const user = req.user;
+
+        if (images.length !== 0) {
+            images.forEach((element, i) => {
+                if (user.images.length !== 5) {
+                    user.images.push(element);
+                } else {
+                    deleteImages.push(element.filename);
+                }
+            });
+        }
+
+        await user.save();
+
+        if (deleteImages.length !== 0) {
+            for(let filename of deleteImages) {
+                await cloudinary.uploader.destroy(filename);
+            };
+            await user.updateOne({
+                $pull: { images: { filename: { $in: deleteImages } } },
+            });
+        }
+
+        res.redirect("images");
     })
 );
 
